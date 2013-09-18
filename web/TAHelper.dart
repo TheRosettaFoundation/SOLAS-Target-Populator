2d005d3bc6d97a882749ad6dd7d82b485096bc4f
Manuel Honegger <manuel_honegger@outlook.com>
2013-09-18 20:29:05 +0100

    Conference updates

--- web/TAHelper.dart
+++ web/TAHelper.dart
@@ -45,37 +45,41 @@ class TAHelper{
      var parser = new DomParser();
      Document xliff =parser.parseFromString(text, 'text/xml');
 //     XmlElement xliff = XML.parse(text);
-     Element root =xliff.$dom_childNodes.first;
+     Element root =xliff.firstChild;
      if(double.parse(root.attributes["version"])==2.0){
-       ElementList transUnits= xliff.queryAll("segment");
-       transUnits.forEach((Element transunit){
-         ElementList altTrans=transunit.queryAll("match");
-         var max=0.0;
-         Element maxElement; 
-         altTrans.forEach((Element alt){
-           if(alt.attributes.containsKey("matchSuitability")&&double.parse(alt.attributes["matchSuitability"])>max){
-             max=alt.attributes["match-quality"];
-             maxElement=alt;
-           }else if(alt.attributes.containsKey("matchQuality")&&alt.attributes.containsKey("similarity")){
-             current = double.parse(alt.attributes["matchQuality"])*double.parse(alt.attributes["matchQuality"]);
-             if(current>max){
-               max=current;
-               maxElement=alt;
+       ElementList units= xliff.queryAll("unit");
+       units.forEach((Element unit){
+         ElementList match=unit.queryAll("match");
+         ElementList segments= unit.queryAll("segment");
+         
+         segments.forEach((Element segment){
+           var max=0.0;
+           Element maxElement;
+           
+           ElementList mrks = segment.queryAll("mrk");
+           Iterable mrkItr = mrks.where((Element e) => e.attributes.containsKey("ref"));
+           mrkItr.forEach((Element mrk) {
+             Element currentMatch=match.singleWhere((Element e)=>"#"+e.attributes["id"]==mrk.attributes["ref"]);
+             if(currentMatch.attributes.containsKey("matchSuitability")&&double.parse(currentMatch.attributes["matchSuitability"])>max){
+               max=currentMatch.attributes["matchSuitability"];
+               maxElement=currentMatch;
+             }else if(currentMatch.attributes.containsKey("matchQuality")&&currentMatch.attributes.containsKey("similarity")){
+               double current = double.parse(currentMatch.attributes["matchQuality"])*double.parse(currentMatch.attributes["similarity"]);
+               if(current>max){
+                 max=current;
+                 maxElement=currentMatch;
+               }
              }
-           }
-         });
-           if(altTrans!=null&& altTrans.isNotEmpty){
-             var placeholder = transunit.query("matches");
-             if(placeholder!=null){
+           });
+           if(match!=null&& match.isNotEmpty){
                Element node=null; 
-               if(maxElement!=null)node = maxElement.query("target");
-               else node = altTrans.first.query("target");
-               if(node.parent.attributes.containsKey("provenanceRecordsRef")){
-                 node =node.clone(true).$dom_setAttribute("its:provenanceRecordsRef", node.parent.attributes["provenanceRecordsRef"]);
-               }else node =node.clone(true);
-               transunit.insertBefore(node,placeholder);
-             }
+               if(maxElement!=null)node = maxElement.query("target").clone(true);
+               else node = match.singleWhere((Element e)=>"#"+e.attributes["id"]==mrks[0].attributes["ref"]).query("target").clone(true);
+               segment.append(node);
            }
+           
+         });
+         
        });
      }else{
        ElementList transUnits= xliff.queryAll("trans-unit");
